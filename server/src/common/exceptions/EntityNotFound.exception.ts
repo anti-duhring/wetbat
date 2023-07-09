187304e3f456cf1821476799db3b207ea902bc66
anti-duhring <mateus.limavn@gmail.com>
2023-07-09 16:34:23 -0300

    feat: Created theme context for Material UI

--- server/src/common/exceptions/EntityNotFound.exception.ts
+++ server/src/common/exceptions/EntityNotFound.exception.ts
@@ -1,6 +1,11 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class EntityNotFoundException extends HttpException {
+  /**
+   * Exception to be thrown when an entity is not found for any reason.
+   * @constructs EntityNotFoundException
+   * @param entityName The name of the entity that was not found. Ex: "Contact" or "Quote"
+   */
   constructor(entityName: string) {
     super(`${entityName} not found`, HttpStatus.NOT_FOUND);
   }
--- server/src/common/exceptions/InvalidData.exception.ts
+++ server/src/common/exceptions/InvalidData.exception.ts
@@ -1,6 +1,12 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class InvalidDataException extends HttpException {
+  /**
+   * Exception to be thrown when the data provided has the right type and the syntax is correct, but it is not valid by a business logic.
+   * Reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422
+   * @constructs InvalidDataException
+   * @param message The error message to be displayed that indicates why the data is invalid
+   */
   constructor(message: string) {
     super(message, HttpStatus.UNPROCESSABLE_ENTITY);
   }
